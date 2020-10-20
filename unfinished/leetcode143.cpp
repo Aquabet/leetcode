24ab3949d837ae739a7206785ceab53c7b823042
Aquabet <897753118@qq.com>
2020-10-20 19:32:04 +0800

    update、

--- unfinished/leetcode143.cpp
+++ unfinished/leetcode143.cpp
@@ -20,10 +20,29 @@ struct ListNode {
 class Solution {
 public:
     void reorderList(ListNode* head){
+        if(head==nullptr) return;
         vector<ListNode*> v;
-        
+        ListNode* node = head;
+        v.push_back(head);
+        while(node->next!=nullptr){
+            v.push_back(node->next);
+            node = node->next;
+        }
+        int flag = 1,phead = 1,pback = v.size()-1;
+        node = head;
+        for(int i = 1;i<v.size();i++){
+            if(flag == 1){
+                node->next = v[pback];
+                pback--;
+                flag = 0;
+            }
+            else if(flag == 0){
+                node->next = v[phead];
+                phead++;
+                flag = 1;
+            }
+            node = node->next;
+        }
+        node->next = nullptr;
     }
 };
-int main(){
-
-}
