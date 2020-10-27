4d1c04b16f80725ab9aed2dbd1feeb96df15b209
Aquabet <897753118@qq.com>
2020-10-27 23:55:55 +0800

    update

---
+++ finished/leetcode9.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s = to_string(x);
+        int i = 0;
+        int j = s.length()-1;
+        while(i <= j){
+            if(s[i] != s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
