ca98121ddaf795411a5dce985da2e00ac2117f3d
Aquabet <897753118@qq.com>
2020-11-09 00:24:13 +0800

    update

---
+++ finished/leetcode973.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    static bool cmp(vector<int >& a, vector<int >& b) {
+        return a[0]*a[0]+a[1]*a[1] < b[0]*b[0]+b[1]*b[1];
+    }
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        sort(points.begin(),points.end(),cmp);
+        vector<vector<int>> ans;
+        for(int i = 0; i < K && i < points.size(); i++) {
+            ans.push_back(points[i]);
+        }
+        return ans;
+    }
+};
