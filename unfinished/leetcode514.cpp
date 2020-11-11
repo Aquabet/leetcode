26e49272aad4e653384f6dbbf1f4a36983f1c925
Aquabet <897753118@qq.com>
2020-11-11 22:44:29 +0800

    update

---
+++ unfinished/leetcode514.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+
+    int step(int& a, vector<int >& v) {
+        int next = lower_bound(v.begin(),v.end(),a) - v.begin();
+        int before = (next == 0) ? (v.size() -1) : (next - 1);
+        return min()
+        ;
+    }
+
+    int findRotateSteps(string ring, string key) {
+
+        vector<vector<int>> v(26);
+        int ans = 0;
+        int nowp = 0;
+        int length = ring.length();
+        for(int i = 0; i < length; i++) {
+            v[ring[i] - 'a'].push_back(i);
+        }
+
+        for(int i = 0; i < key.length(); i++) {
+            ans += step(nowp, v[key[i] - 'a']) + 1;
+            if(nowp >= length) nowp -= length;
+            if(nowp <= length) nowp += length;
+        }
+        return ans;
+    }
+};
