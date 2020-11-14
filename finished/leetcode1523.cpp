e30e7bba868f26ffa4a1b26cf04d7cc787c992d9
Aquabet <897753118@qq.com>
2020-11-14 15:42:54 +0800

    update

---
+++ finished/leetcode1523.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if(low % 2 == 0 && high % 2 == 0) return (high-low)/2;
+        return ((high-low)/2)+1;
+    }
+};
