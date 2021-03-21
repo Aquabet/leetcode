b5342e58a6e605a2f015b0dea5919536db506de1
Aquabet <897753118@qq.com>
2021-03-21 17:04:26 +0800

    update

---
+++ unfinished/leetcode115.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int lens = s.length();
+        int lent = t.length();
+        int dp[lens][lent];
+        memset(dp, 0, sizeof(dp));
+        for(int i = 0; i < lens; i++) {
+            for(int j = 0; j < lent; j++) {
+                
+            }
+        }
+    }
+};
