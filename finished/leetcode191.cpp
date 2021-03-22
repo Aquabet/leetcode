248a537dc628bc8b06015af2c48749acf9fd792d
Aquabet <897753118@qq.com>
2021-03-22 11:02:51 +0800

    update

---
+++ finished/leetcode191.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        while(n != 0) {
+            cnt += n % 2;
+            n /= 2;
+        }
+        return cnt;
+    }
+};
