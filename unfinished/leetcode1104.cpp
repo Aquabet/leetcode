a27dc75feffad578ae2834b72abcc5d3094782dd
Aquabet <897753118@qq.com>
2021-07-29 16:57:47 +0800

    update

---
+++ unfinished/leetcode1104.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> pathInZigZagTree(int label) {
+        int level = 0;
+        int l = label;
+        vector<int > levelv;
+        while(l) {
+            l /= 2;
+            level++;
+        }
+    }
+};
+//数学题
