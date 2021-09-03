e05d0885bdbedbd140733abbe4434c36b6281411
Aquabet <897753118@qq.com>
2021-09-03 10:29:39 +0800

    update

---
+++ finished/leetcode17.14.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<int> smallestK(vector<int>& arr, int k) {
+        vector<int> ans;
+        sort(arr.begin(), arr.end() );
+        for(int i = 0; i < k; i++) {
+            ans.push_back(arr[i]);
+        }
+        return ans;
+    }
+};
