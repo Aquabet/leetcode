263c298a5acc9c8c9a37d66e197bc6850e4a3fda
Aquabet <897753118@qq.com>
2021-09-09 14:11:57 +0800

    update

--- rbq/Mode.cpp
+++ rbq/Mode.cpp
@@ -9,6 +9,13 @@ using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 ll gcd(ll a, ll b) {
     return b?gcd(b,a%b):a;
 }
@@ -48,7 +55,51 @@ ll kasumi(ll a, ll b, ll mod) {
     }
     return ans % mod;
 }
+//二叉树的序列化
+string serialize(TreeNode* root) {
+        if (!root) {
+            return "X";
+        }
+        auto left = "(" + serialize(root->left) + ")";
+        auto right = "(" + serialize(root->right) + ")";
+        return left + to_string(root->val) + right;
+    }
 
+inline TreeNode* parseSubtree(const string &data, int &ptr) {
+    ++ptr; // 跳过左括号
+    auto subtree = parse(data, ptr);
+    ++ptr; // 跳过右括号
+    return subtree;
+}
+
+inline int parseInt(const string &data, int &ptr) {
+    int x = 0, sgn = 1;
+    if (!isdigit(data[ptr])) {
+        sgn = -1;
+        ++ptr;
+    }
+    while (isdigit(data[ptr])) {
+        x = x * 10 + data[ptr++] - '0';
+    }
+    return x * sgn;
+}
+
+TreeNode* parse(const string &data, int &ptr) {
+    if (data[ptr] == 'X') {
+        ++ptr;
+        return nullptr;
+    }
+    auto cur = new TreeNode(0);
+    cur->left = parseSubtree(data, ptr);
+    cur->val = parseInt(data, ptr);
+    cur->right = parseSubtree(data, ptr);
+    return cur;
+}
+//二叉树的反序列化
+TreeNode* deserialize(string data) {
+    int ptr = 0;
+    return parse(data, ptr);
+}
 
 class Solution {
 public:
